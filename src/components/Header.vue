7f055279d459ccd3a21b3deeb525dc93bd6b622b
fanhaipeng <fanhaipeng0403@gmail.com>
2018-06-21 22:56:33 +0800

    '为Vue配置stylus-https://blog.csdn.net/weixin_37595918/article/details/70577720'

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,54 @@
 <template>
+
   <div class="header">
+    <div class="header-inner">
+
+      <div class="site-log">
+        <h1><a href="" class="link-logo"></a></h1>
+      </div>
+
+      <ul class="quick-menu">
+
+        <li class="search"></li>
+        <li class="message"></li>
+        <li class="editor"></li>
+        <li class="user"></li>
+      </ul>
+
+    </div>
+    <div class="nav">
+
+    </div>
 
   </div>
 
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+  export default {
+    name: "Header"
+  }
 </script>
 
-<style scoped>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+  .container
+    width: 100%
+    height: 136px
+    position: fixed
+    top: 0
+    .header
+      width: 100%
+      height: 88px
+      font-size: 16px;
+      color: #333;
+      .site-log
+        width: 89px
+        height: 46px
+        float: left
+        background: url("assets/image/logo@2x.png")
+      .quick-menu
 
 </style>
+
