81af1ef60e540feff7d7c9f9451a0ffa71bc0eff
fanhaipeng <fanhaipeng0403@gmail.com>
2018-06-24 00:37:44 +0800

    ‘postion-display-float----

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,18 @@
         <li class="search">
           <input type="text">
         </li>
-        <li class="message"></li>
-        <li class="editor"></li>
-        <li class="user"></li>
+        <li class="message">
+          <img class="logo" :src="message" alt="由心造">
+        </li>
+        <li class="editor">
+
+          <img class="logo" :src="editor" alt="由心造">
+        </li>
+        <li class="user">
+
+          <img class="logo" :src="avatar" alt="由心造">
+
+        </li>
       </ul>
 
     </div>
@@ -33,14 +42,21 @@
   // @1x @2x和@3x严格来说是苹果公司的专利。
   // 有的地方就是没有，并不一定需要填充div
 
-  import logo from '@/assets/site-log.png'
-
+  import logo from '@/assets/image/site-log.png'
+  import message from '@/assets/image/message.png'
+  import popular from '@/assets/image/popular.png'
+  import editor from '@/assets/image/editor.png'
+  import avatar from '@/assets/image/avatar.png'
 
   export default {
     name: "Header",
     data() {
       return {
         logo,
+        message,
+        popular,
+        editor,
+        avatar,
       }
     }
 
@@ -54,6 +70,7 @@
     width 100%
     height 88px
     display flex
+    justify-content space-between
     &:after
       box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.07);
       display: block
@@ -63,12 +80,14 @@
       width: 100%;
       height: 2px;
     a
-
       .logo
         width 89px
         height 46px
-        /*margin 15px 10px 0px 25px*/
+        margin 15px 10px 0px 25px
     .quick-menu
+      display flex
+      justify-content space-around
+
         /*position flex*/
 
 
