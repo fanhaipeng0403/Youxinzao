39d72379ee72e15f87280b5be3d9df2d8346b4d2
fanhaipeng <fanhaipeng0403@gmail.com>
2018-07-20 19:59:03 +0800

    'header完成学习transcation'

--- src/common/Header.vue
+++ src/common/Header.vue
@@ -1,64 +1,107 @@
 <template>
-  <header style="top: 0px; padding-bottom: 54px;">
+  <header :class="[isScrollTop ? 'header-mini' : 'header']">
 
-    <nav id="mainnavi">
-      <h1 class="logo"><a href="http://muuuuu.org/"><img style="width: 170px; height: 21px"
-                                                         src="http://muuuuu.org/common/img/logo.png"></a></h1>
+    <nav>
+      <h1 class="logo"><a href="http://muuuuu.org/"><img
+        src="http://muuuuu.org/common/img/logo.png"></a></h1>
       <ul>
-        <li><a href="http://muuuuu.org/">阅览</a></li>
-        <li><a href="/about/">游记</a></li>
-        <li><a href="http://muuuuu.org/">移民</a></li>
-        <li><a class="current" href="/category" style="padding-bottom: 54px;">国家</a></li>
-        <li><a href="https://www.facebook.com/muuuuu.chang" target="_blank">论坛</a></li>
+        <li><a :class="[isScrollTop?  'link-mini':'link']" href="http://muuuuu.org/">阅览</a></li>
+        <li><a :class="[isScrollTop? 'link-mini':'link']" href="/about/">游记</a></li>
+        <li><a :class="[isScrollTop? 'link-mini':'link']" href="http://muuuuu.org/">移民</a></li>
+        <li><a :class="[isScrollTop?  'link-mini':'link']" href="/category">国家</a></li>
+        <li><a :class="[isScrollTop? 'link-mini':'link']" href="https://www.facebook.com/muuuuu.chang"
+               target="_blank">论坛</a></li>
       </ul>
     </nav>
 
   </header>
+
 </template>
 
 <script>
-
-  // @1x @2x和@3x严格来说是苹果公司的专利。
-  // 有的地方就是没有，并不一定需要填充div
 
 
   export default {
     name: "Header",
     data() {
-      return {}
-    }
+      return {
+        isScrollTop: 0
+      }
+    },
+    methods: {
 
+      handleScroll() {
+        this.isScrollTop = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > 20
+      }
+
+    },
+    mounted() {
+
+      window.addEventListener('scroll', this.handleScroll, true)
+    }
   }
 </script>
 
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
   header
-    background: url(../assets/image/navibg.png) no-repeat scroll left bottom rgba(0, 0, 0, 0);
-    left: 50%;
-    margin: 0 auto 60px -713px;
-    overflow: hidden;
-    padding-bottom: 54px;
-    padding-top: 90px;
-    position: fixed;
-    top: 0;
-    width: 1425px;
-    z-index: 99999;
-
+   transition:all 1300ms
   .logo
     display inline-block
 
   ul
     display inline-block
+    margin-left: 72px;
     li
       display inline-block
-      a
-        text-decoration: none
-        color: #333
-        margin-right: 25px;
-        font-size: 12px;
-        letter-spacing: 0.1em
-        font-weight: 400
+
+  img
+    width 170px
+    height 21px
+
+  .header-mini
+    background: url(../assets/image/navibg.png) no-repeat scroll left bottom rgba(0, 0, 0, 0);
+    left: 14.5%;
+    overflow: hidden;
+    padding-bottom: 0;
+    padding-top: 40px;
+    position: fixed;
+    top: -65px;
+    width: 80%;
+    z-index: 99999;
+
+  .header
+    background: url(../assets/image/navibg.png) no-repeat scroll left bottom rgba(0, 0, 0, 0);
+    left: 14.5%;
+    overflow: hidden;
+    padding-bottom: 15px;
+    padding-top: 40px;
+    position: fixed;
+    top: 0;
+    width: 80%;
+    z-index: 99999;
+
+  .link-mini
+    text-decoration: none
+    color: #333
+    margin-right: 30px;
+    font-size: 13px;
+    letter-spacing: 0.1em
+    font-weight: 400
+    padding-bottom: 1.3rem
+    &:hover
+      border-bottom: 1px solid #ec652b
+
+  .link
+    text-decoration: none
+    color: #333
+    margin-right: 30px;
+    font-size: 13px;
+    letter-spacing: 0.1em
+    font-weight: 400
+    padding-bottom: 2.3rem
+    &:hover
+      border-bottom: 1px solid #ec652b
 
 
 </style>
