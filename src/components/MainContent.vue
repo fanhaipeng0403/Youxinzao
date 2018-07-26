14c22799b217129b16c2d0cfdbfb561869bdc09a
fanhaipeng <fanhaipeng0403@gmail.com>
2018-07-26 18:31:53 +0800

    '使用源码vue-waterfall‘

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,23 +1,23 @@
 <template>
 
-  <section class="content">
 
-  <vue-waterfall-easy :imgsArr="imgsArr" @scrollReachBottom="getData"></vue-waterfall-easy>
+    <section class="content">
 
-  </section>
+      <vue-waterfall-easy :imgsArr="imgsArr" @scrollReachBottom="getData"></vue-waterfall-easy>
+
+    </section>
 
 </template>
 
 <script>
-  import vueWaterfallEasy from 'vue-waterfall-easy'
+  import vueWaterfallEasy from '@/components/vue-waterfall-easy/vue-waterfall-easy'
+  import {mapGetters} from 'vuex'
 
   export default {
     name: 'MainContent',
     data() {
       return {
-        imgsArr: [
-          {
-            "src": "static/img/1.jpg",
+        imgsArr: [ { "src": "static/img/1.jpg",
             "href": "https://www.baidu.com",
             "info": "一些图片描述文字"
           },
@@ -126,13 +126,108 @@
     },
     methods: {
       getData() {
-        // In the real environment,the backend will return a new image array based on the parameter group.
-        // Here I simulate it with a stunned json file.
-        // axios.get('../../node_modules/vue-waterfall-easy/demo/static/mock/data.json?group=' + this.group)
-        //   .then(res => {
-        //     this.imgsArr = this.imgsArr.concat(res.data)
-        //     this.group++
-        //   })
+        this.imgsArr = this.imgsArr.concat(
+         [ {
+            "src": "static/img/1.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/2-.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/3.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/4.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/5.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/6.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/7.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/8.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/9.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/10.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/11.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/12.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/13.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/14.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/15.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/16.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/17.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/18.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/19.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          },
+          {
+            "src": "static/img/20.jpg",
+            "href": "https://www.baidu.com",
+            "info": "一些图片描述文字"
+          }
+        ] )
       },
     },
     created() {
@@ -141,10 +236,10 @@
   }
 </script>
 
- <style lang="stylus" rel="stylesheet/stylus" scoped>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
   .content
     height: 100vh;
-    margin-top: 146px;
-    overflow: visible;
-
+    margin: 146px 0 0 0
+  .blur
+    filter: blur(2px);
 </style>
